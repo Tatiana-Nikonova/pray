14db8d1e1042462fcc9ba4746cdbd3141d229bd3
Tatiana Nikonova <taniushka_2007@mail.ru>
2022-04-29 12:59:35 +0300

    Initial

---
+++ src/components/Bogorodice.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Bogorodice() {
+	return (
+		<div>
+			<h1>Богородичное правило</h1>
+			
+		</div>
+	)
+}
+export default Bogorodice;
---
+++ src/components/Otrok.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Otrok() {
+	return (
+		<div>
+			<h1>Акафист отроку Вячеславу</h1>
+		</div>
+	)
+}
+export default Otrok;
